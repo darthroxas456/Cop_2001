002ffd2e3e07fbff77bc0cdb403fa1a85ad80c23
owenr <darthroxas456@gmail.com>
2019-05-15 12:28:29 -0400

    added things for Procedural Project 1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Production Line Tracker\n";
+    std::cout << "\n";
+    std::cout << "1. Add Employee Account\n";
+    std::cout << "2. Add Music Player\n";
+    std::cout << "3. Add Movie Player\n";
+    std::cout << "4. Display Production Statistics\n";
+    std::cout << "5. Exit\n";
     return 0;
 }
