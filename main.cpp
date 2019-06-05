ea18d6fa7418219606899cb9eae323d938be7a2c
owenr <darthroxas456@gmail.com>
2019-06-05 19:14:32 -0400

    complete overhaul

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <sstream>
 //prototype
 using namespace std;
 
@@ -21,12 +22,6 @@ using namespace std;
  *  The showMenu function creates the menu that the user will use to travel through
  *  to create serial numbers for the media players made.
  *
- *  @param  "1. Produce Items";
-    @param  "2. Add Employee Account";
-    @param  "3. Add Music Player";
-    @param  "4. Add Movie Player";
-    @param  "5. Display Production Statistics";
-    @param  "6. Exit";
  *
  *  @return void
  */
@@ -38,63 +33,83 @@ void showMenu();
  *  item name, and what type of product the item is and then prints out
  *  in a single line what they inputted.
  *
- *  @param input of manufacturer
- *  @param input of product name
- *  @param input of product type.
- *  @param output of the three listed above as a string.
+ *  @param productLineManufacturer: this vector contains
  *
  *  @return void
  */
-void addToProductLine(std::vector<std::string>&, std::vector<std::string>&,
-                      std::vector<std::string>&);
+void addToProductLine(std::vector<std::string> &,
+                      std::vector<std::string> &,
+                      std::vector<std::string> &);
 
-/** @brief This function prints out the sample information the user can input.
+/** @brief This function prints out a string of code.
  *
- *  The add_sample_products function used vectors to create
- *  a grid of data to print out sample data for the user.
+ *  the addToProductLine functions allows the user to input the manufacturer,
+ *  item name, and what type of product the item is and then prints out
+ *  in a single line what they inputted.
  *
- *  @param product_line_manufacturers.push_back("Microsoft");
- *  @param product_line_names.push_back("Zune");
- *  @param product_line_item_types.push_back("AM");
- *  @param product_line_manufacturers.push_back("Apple");
- *  @param product_line_names.push_back("iPod");
- *  @param product_line_item_types.push_back("AM");
- *  @param product_line_manufacturers.push_back("Sylvania");
- *  @param product_line_names.push_back("SDVD1187");
- *  @param product_line_item_types.push_back("VM");
+ *  @param
  *
  *  @return void
  */
-void add_sample_products(std::vector<std::string> &, std::vector<std::string> &,
-                         std::vector<std::string> &);
+void production_log(std::vector<int> &production_number,
+                    std::vector<std::string> &production_manufacturers,
+                    std::vector<std::string> &production_names,
+                    std::vector<std::string> &production_item_types,
+                    std::vector<std::string> &production_serial_num,
+                    std::vector<std::string> manufacturers,
+                    std::vector<std::string> names,
+                    std::vector<std::string> item_types,
+                    int &MM_num, int &AM_num,
+                    int &VI_num, int &VM_num);
 
-void output_product_line(std::vector<std::string>, std::vector<std::string>,
-                         std::vector<std::string>);
+
+void add_sample_products(std::vector<std::string> &manufacturers,
+                         std::vector<std::string> &names,
+                         std::vector<std::string> &item_types);
 
 void output_sorted_product_names(std::vector<std::string>);
 
-void find_manufacturer_of_product(std::vector<std::string>, std::vector<std::string>);
+void find_manufacturer_of_product();
 
-
-int prodNum = 1;
-int audioSerialNum = 1;
-int audioMobileSerialNum = 1;
-int visualSerialNum = 1;
-int visualMobileSerialNum = 1;
 
 int main() {
     int choice; //to hold the menu choice.
 
+    int AM_num = 0;
+    int MM_num = 0;
+    int VI_num = 0;
+    int VM_num = 0;
+    // vectors for Product Line
+    // vector to store product manufacturer
+    std::vector<std::string> manufacturers;
+    // a parallel vector to store product name
+    std::vector<std::string> names;
+    // a parallel vector to store product item type
+    std::vector<std::string> item_types;
+
+    //Production vectors
+    std::vector<int> production_number;
+
+    std::vector<std::string> production_manufacturers;
+
+    std::vector<std::string> production_names;
+
+    std::vector<std::string> production_item_types;
+
+    std::vector<std::string> production_serial_num;
+
     //these are the constants for each of the choices.
     const int PRODUCE_ITEMS = 1,
             ADD_EMPLOYEE_ACCOUNT = 2,
-            ADD_MUSIC_PLAYER = 3,
-            ADD_MOVIE_PLAYER = 4,
-            DISPLAY_PRODUCTION_STATISTICS = 5,
-            QUIT_CHOICE = 6;
+            ADD_ITEM = 3,
+            DISPLAY_PRODUCTION_STATISTICS = 4,
+            QUIT_CHOICE = 5;
+
+    add_sample_products(manufacturers, names, item_types);
 
     std::cout << "Production Line Tracker\n";
     std::cout << "\n";
+
     do {
         //Display the menu and get user's choice.
         showMenu();
@@ -103,10 +118,9 @@ int main() {
             std::cout << "Not a valid selection\n";
             std::cout << "1. Produce Items\n";
             std::cout << "2. Add Employee Account\n";
-            std::cout << "3. Add Music Player\n";
-            std::cout << "4. Add Movie Player\n";
-            std::cout << "5. Display Production Statistics\n";
-            std::cout << "6. Exit\n";
+            std::cout << "3. Add Item\n";
+            std::cout << "4. Display Production Statistics\n";
+            std::cout << "5. Exit\n";
             std::cin >> choice;
         }
         // If the user doesn't want to quit, proceed.
@@ -114,19 +128,28 @@ int main() {
             //display the menu STUBS.
             switch (choice) {
                 case PRODUCE_ITEMS:
-                    std::cout << "Produce Items Stub\n"; //prints a stub for the selected item
+                    //std::cout << "Produce Items Stub\n"; //prints a stub for the selected item
+                    production_log(production_number,
+                                   production_manufacturers,
+                                   production_names,
+                                   production_item_types,
+                                   production_serial_num,
+                                   manufacturers,
+                                   names,
+                                   item_types,
+                                   MM_num, AM_num,
+                                   VI_num, VM_num);
                     break;
                 case ADD_EMPLOYEE_ACCOUNT:
                     std::cout << "Add Employee Account Stub\n"; //prints a stub for the selected item
                     break;
-                case ADD_MUSIC_PLAYER:
-                    std::cout << "Add Music Player Stub\n"; //prints a stub for the selected item
-                    break;
-                case ADD_MOVIE_PLAYER:
-                    std::cout << "Add Movie Player Stub\n"; //prints a stub for the selected item
+                case ADD_ITEM:
+                    addToProductLine(manufacturers, names, item_types);
                     break;
                 case DISPLAY_PRODUCTION_STATISTICS:
-                    std::cout << "Display Production Statistics Stub\n"; //prints a stub for the selected item
+                    //std::cout << "Display Production Statistics Stub\n"; //prints a stub for the selected item
+                    output_sorted_product_names(names);
+                    find_manufacturer_of_product();
                     break;
                 default:
                     std::cout << "Please select an item from the menu. " << std::endl;
@@ -135,50 +158,24 @@ int main() {
         }
     } while (choice != QUIT_CHOICE);
 
+}
 
-    // Create vectors
-    // vector to store product manufacturer
-    std::vector<std::string> productLineManufacturer;
-    // a parallel vector to store product name
-    std::vector<std::string> productLineName;
-    // a parallel vector to store product item type
-    // create vector here
-    std::vector<std::string> productLineItemType;
-    // Add three new products to the product line
-
-    addToProductLine(productLineManufacturer, productLineName, productLineItemType);
-    addToProductLine(productLineManufacturer, productLineName, productLineItemType);
-    addToProductLine(productLineManufacturer, productLineName, productLineItemType);
-
-    // Output the products in the product line
-    std::cout << "The products in the Product Line are:\n";
-
-    for (int productLineItemNum=0; productLineItemNum < 3; productLineItemNum++) {
-        std::cout << productLineManufacturer[productLineItemNum] << ", ";
-        std::cout << productLineName[productLineItemNum] << ", ";
-        std::cout << productLineItemType[productLineItemNum] << "\n";
-    }
-
-    // vectors for Product Line
-    // vector to store product manufacturer
-    std::vector<std::string> product_line_manufacturers;
-    // a parallel vector to store product name
-    std::vector<std::string> product_line_names;
-    // a parallel vector to store product item type
-    std::vector<std::string> product_line_item_types;
-
-    add_sample_products(product_line_manufacturers, product_line_names, product_line_item_types);
-    output_product_line(product_line_manufacturers, product_line_names, product_line_item_types);
-    output_sorted_product_names(product_line_names);
-    output_product_line(product_line_manufacturers, product_line_names, product_line_item_types);
-    return 0;
+//***********************************************************
+//Definition of function showMenu which displays the menu. **
+//***********************************************************
+void showMenu() {
+    std::cout << "\nProduction Line Tracker\n";
+    std::cout << "1. Produce Items\n";     // these are the choices for the initial menu the user sees.
+    std::cout << "2. Add Employee Account\n";
+    std::cout << "3. Add Item\n";
+    std::cout << "4. Display Production Statistics\n";
+    std::cout << "5. Exit\n";
 }
 
 void addToProductLine(std::vector<std::string> &productLineManufacturer,
-                      std::vector<std::string> &productLineName, std::vector<std::string> &productLineItemType)
-{
+                      std::vector<std::string> &productLineName, std::vector<std::string> &productLineItemType) {
     std::cout << "Adding a new product to the product line\n";
-
+    std::cin.ignore();
     std::cout << "Enter the Manufacturer\n";
     std::string manufacturer;
     getline(std::cin, manufacturer);
@@ -186,18 +183,23 @@ void addToProductLine(std::vector<std::string> &productLineManufacturer,
 
     productLineManufacturer.push_back(manufacturer);
 
+
     std::cout << "Enter the Product Name\n";
     std::string prodName;
     getline(std::cin, prodName);
     // add prodName to the vector
-    productLineName.push_back (prodName);
+    productLineName.push_back(prodName);
+
 
     std::cout << "Enter the item type\n";
-    std::cout << "1. Audio\n" <<
+    std::cout <<
+              "1. Audio\n" <<
               "2. Visual\n" <<
-              "3. AudioMobile\n" <<
-              "4. VisualMobile\n";
+              "3. Audio Mobile\n" <<
+              "4. Visual Mobile\n";
+
     int itemTypeChoice;
+
     std::cin >> itemTypeChoice;
     std::string itemTypeCode;
     switch (itemTypeChoice) {
@@ -220,92 +222,116 @@ void addToProductLine(std::vector<std::string> &productLineManufacturer,
             break;
     }
     // add itemTypeCode to the vector
-    productLineItemType.push_back (itemTypeCode);
+    productLineItemType.push_back(itemTypeCode);
     std::cin.ignore();
-    cout << "Enter the number of items that were produced\n";
-    int numProduced;
-    cin >> numProduced; // assigns what the user input to numProduced.
 
-    // add a loop to record production, for now simply by
-    // outputting production number and serial number
-    int i;
-    for (i = 1; i <= numProduced; i++) {
-        cout << "Production Number: " << i << " Serial Number: "<< manufacturer << itemTypeCode << "0000" << i << endl;
-    }    // prints out the production numbers and serial numbers for what the user asked for.
+    cout << manufacturer << ", " << prodName << ", " << itemTypeCode << endl << endl;
 
 }
 
-void add_sample_products(std::vector<std::string> &product_line_manufacturers,
-                         std::vector<std::string> &product_line_names, std::vector<std::string> &product_line_item_types) {
-    product_line_manufacturers.push_back("Microsoft");
-    product_line_names.push_back("Zune");
-    product_line_item_types.push_back("AM");
-    product_line_manufacturers.push_back("Apple");
-    product_line_names.push_back("iPod");
-    product_line_item_types.push_back("AM");
-    product_line_manufacturers.push_back("Sylvania");
-    product_line_names.push_back("SDVD1187");
-    product_line_item_types.push_back("VM");
+void production_log(std::vector<int> &production_number,
+                    std::vector<std::string> &production_manufacturers,
+                    std::vector<std::string> &production_names,
+                    std::vector<std::string> &production_item_types,
+                    std::vector<std::string> &production_serial_num,
+                    std::vector<std::string> manufacturers,
+                    std::vector<std::string> names,
+                    std::vector<std::string> item_types,
+                    int &MM_num, int &AM_num,
+                    int &VI_num, int &VM_num) {
 
-}
-
-void output_product_line(std::vector<std::string> product_line_manufacturers,
-                         std::vector<std::string> product_line_names, std::vector<std::string> product_line_item_types) {
-    for (int product_index = 0; product_index < 3; product_index++) {
-        std::cout << product_line_manufacturers[product_index] << ",";
-        std::cout << product_line_names[product_index] << ",";
-        std::cout << product_line_item_types[product_index] << "\n";
+    int prodLine_num;
+    cout << "Select one of the following:" << endl;
+    for (prodLine_num = 0; prodLine_num < manufacturers.size();
+         prodLine_num++) {
+        cout << prodLine_num + 1 << ". " << manufacturers[prodLine_num]
+             << ", " << names[prodLine_num] << endl;
     }
+    int choice;
+    cin >> choice;
+
+    int amount_made;
+    cout << "Amount Made:" << endl;
+    cin >> amount_made;
+    for (int i = 0; i < amount_made; i++) {
+        production_number.push_back(prodLine_num + 1);
+
+        production_manufacturers.push_back(manufacturers[choice - 1]);
+
+        production_names.push_back(names[choice - 1]);
+
+        production_item_types.push_back(item_types[choice - 1]);
+
+        int serial_num;
+        if (item_types[choice - 1] == "AM") {
+            serial_num = AM_num;
+            AM_num++;
+        } else if (item_types[choice - 1] == "MM") {
+            serial_num = MM_num;
+            MM_num++;
+        } else if (item_types[choice - 1] == "VI") {
+            serial_num = VI_num;
+            VI_num++;
+        } else if (item_types[choice - 1] == "VM") {
+            serial_num = VM_num;
+            VM_num++;
+        }
+        std::stringstream serial;
+        serial << production_manufacturers[choice - 1] + item_types[choice - 1] << setfill('0') << setw(5)
+               << serial_num;
+        production_serial_num.push_back(serial.str());
+
+        cout << serial.str() << endl;
+    }
+
+
 }
 
-void output_sorted_product_names(std::vector<std::string> product_line_names) {
-    sort(product_line_names.begin(), product_line_names.end());
+void add_sample_products(std::vector<std::string> &manufacturers,
+                         std::vector<std::string> &names,
+                         std::vector<std::string> &item_types) {
+    manufacturers.push_back("Microsoft");
+    names.push_back("Zune");
+    item_types.push_back("AM");
+    manufacturers.push_back("Apple");
+    names.push_back("iPod");
+    item_types.push_back("AM");
+    manufacturers.push_back("Sylvania");
+    names.push_back("SDVD1187");
+    item_types.push_back("VM");
 
-    for (auto x : product_line_names) {
+}
+
+void output_sorted_product_names(std::vector<std::string> names) {
+    sort(names.begin(), names.end());
+
+    for (auto x : names) {
         std::cout << x << std::endl;
     }
 
 }
 
-void find_manufacturer_of_product(std::vector<std::string> product_line_manufacturers,
-                                  std::vector<std::string> product_line_names) {
+void find_manufacturer_of_product() {
 
-    std::cout << "Enter a product name to find the manufacturer\n";
+    std::cout << "Enter the product name from the list above "
+                 "to find the manufacturer\n";
     std::string prod_name_to_find;
     std::cin >> prod_name_to_find;
 
     if (prod_name_to_find == "iPod") {
         std::cout << "The manufacturer of that product is Apple" << std::endl;
-    } else if (prod_name_to_find == "ipod"){
+    } else if (prod_name_to_find == "ipod") {
         std::cout << "The manufacturer of that product is Apple" << std::endl;
-    }else if(prod_name_to_find == "Zune") {
+    } else if (prod_name_to_find == "Zune") {
         std::cout << "The manufacturer of that product is Microsoft" << std::endl;
-    }else if(prod_name_to_find == "zune") {
+    } else if (prod_name_to_find == "zune") {
         std::cout << "The manufacturer of that product is Microsoft" << std::endl;
-    }else if(prod_name_to_find == "SDVD1187") {
+    } else if (prod_name_to_find == "SDVD1187") {
         std::cout << "The manufacturer of that product is Sylvania" << std::endl;
-    }else if(prod_name_to_find == "sdvd1187") {
+    } else if (prod_name_to_find == "sdvd1187") {
         std::cout << "The manufacturer of that product is Sylvania" << std::endl;
-    }else{
-        std::cout << "That product name was not found." << std::endl;
+    } else {
+        std::cout << "The product name was not found." << std::endl;
     }
-
-
-
-
 }
-
-//***********************************************************
-//Definition of function showMenu which displays the menu. **
-//***********************************************************
-void showMenu() {
-
-    std::cout << "1. Produce Items\n";     // these are the choices for the initial menu the user sees.
-    std::cout << "2. Add Employee Account\n";
-    std::cout << "3. Add Music Player\n";
-    std::cout << "4. Add Movie Player\n";
-    std::cout << "5. Display Production Statistics\n";
-    std::cout << "6. Exit\n";
-}
-
 
